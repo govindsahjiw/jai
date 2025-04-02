e406b6303db478f70819c009650f54d12cbcf945
govindsahjiw <govind.sah@jaiinfoway.com>
2025-04-02 09:53:44 +0530

    Border Fotter Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="py-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         {/* Copyright */}
-        <div className="border-t border-gray-800 mt-8 pt-6 text-center text-sm text-gray-500">
+        <div className="border-t border-gray-200 mt-8 pt-6 text-center text-sm text-gray-500">
           <p>© {new Date().getFullYear()} Jai Infoway Pvt. Ltd. All rights reserved.</p>
         </div>
       </div>
