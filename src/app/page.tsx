e3dca8742734b4801dd3d366eb218d72c074719c
govindsahjiw <govind.sah@jaiinfoway.com>
2025-04-03 19:08:13 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
             e.currentTarget.src = "/img/fallback-loader.svg";
           }}
         />
-        <p className="mt-3 text-gray-500">Welcome To JaiInfoWay...</p>
+        <p className="mt-3 text-gray-500">Welcome To Jai-Info Way...</p>
       </div>
     </div>
   );
