b5fb5177d7c76b068a3b7b64b97f7dff30746447
govindsahjiw <govind.sah@jaiinfoway.com>
2025-04-04 18:26:26 +0530

    update text  to voice feature

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import Avatar from "../../public/img/avatar.webp";
 import Link from 'next/link';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import useClipboard from "react-use-clipboard";
+
 import { useState, useEffect } from "react";
 
 interface Slide {
@@ -47,10 +47,7 @@ export default function HeroSection({
   const [inputText, setInputText] = useState("");
   const [isListening, setIsListening] = useState(false);
   const [textToCopy, setTextToCopy] = useState<string>("");
-  const [isCopied, setCopied] = useClipboard(textToCopy, {
-    successDuration: 1000
-  });
-
+ 
   if (!heroData) return null;
 
   const benefitToConversationMap: Record<string, string> = {
@@ -133,11 +130,7 @@ export default function HeroSection({
     }
   };
 
-  const handleCopy = () => {
-    setTextToCopy(inputText);
-    setCopied();
-  };
-
+  
   const handleFreeTrialSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsSubmitting(true);
@@ -369,22 +362,6 @@ export default function HeroSection({
                 <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex gap-1">
                   {inputText && (
                     <>
-                      <button
-                        onClick={handleCopy}
-                        className="p-1 text-gray-500 hover:text-blue-600 transition-colors"
-                        title="Copy"
-                      >
-                        {isCopied ? (
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                          </svg>
-                        ) : (
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
-                            <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z" />
-                          </svg>
-                        )}
-                      </button>
                       <button
                         onClick={handleReset}
                         className="p-1 text-gray-500 hover:text-red-600 transition-colors"
